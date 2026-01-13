6ed31befbd9ea5655c0e73dea2e50571b2fc7825
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2026-01-13 13:31:16 +0100

    Improve audit_log benchmarking

--- audit_log/run_audit_log_commercial.sh
+++ audit_log/run_audit_log_commercial.sh
@@ -1,10 +1,11 @@
 SCRIPT_DIR=/data/pstress/pstress/scripts
 AUDIT_DIR=/data/db-bench/audit_log
-DATA_DIR=/mnt/black/pstress-run/workdir-8044/audit_log_filter/data_alf_84
+DATA_DIR=/mnt/black/pstress-run/audit_log_filter/data_alf_84
 
 BASEDIR=/data/mysql-server/mysql-8.4.7-commercial
 #COMMON_SQL=common_single_table.sql
-COMMON_SQL=udf_audit_log_format.sql 
+#COMMON_SQL=udf_audit_log_format.sql
+COMMON_SQL=check_table_format.sql
 
 $SCRIPT_DIR/mysql_option_tester.py \
    --basedir $BASEDIR \
